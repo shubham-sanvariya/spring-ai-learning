25bb23be4cf5f5f52fcfbe447dde285e63ac681c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-02-20 17:27:07 +0530

    first commit

---
+++ lessaon1/src/main/java/com/ai/lessaon1/Lessaon1Application.java
@@ -0,0 +1,13 @@
+package com.ai.lessaon1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lessaon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lessaon1Application.class, args);
+	}
+
+}
---
+++ lessaon1/src/test/java/com/ai/lessaon1/Lessaon1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.lessaon1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lessaon1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
