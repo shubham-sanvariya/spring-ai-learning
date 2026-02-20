1956b6bcea2ca7c90c96d42ca74ae932d9c1915c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-02-20 19:32:16 +0530

    Add ChatController and application properties for AI chat configuration

---
+++ lessaon1/src/main/java/com/ai/lessaon1/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.ai.lessaon1.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+    
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam(value = "q") String query){
+        String resContent = this.chatClient.prompt(query).call().content();
+
+        return ResponseEntity.ok(resContent);
+    }
+}
