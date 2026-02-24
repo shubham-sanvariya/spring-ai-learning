640a16c3171aac95c0624ef1e78051057ac40fc2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-02-24 15:42:44 +0530

    adding chat service with method chattemplate

---
+++ lesson2_advisors/src/main/java/com/ai/lesson2_advisors/service/ChatService.java
@@ -0,0 +1,7 @@
+package com.ai.lesson2_advisors.service;
+
+
+public interface ChatService {
+    
+    String chatTemplate(String query);
+}
---
+++ lesson2_advisors/src/main/java/com/ai/lesson2_advisors/service/ChatServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ai.lesson2_advisors.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+
+
+public class ChatServiceImpl implements ChatService{
+
+
+    private ChatClient chatClient;
+
+    @Value("classpath:/prompts/user-message.st")
+    private Resource userMessage;
+
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemMessage;
+
+    public ChatServiceImpl(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+
+
+    @Override
+    public String chatTemplate(String query) {
+        return this.chatClient
+
+                .prompt()
+                // .advisors(new SimpleLoggerAdvisor())
+                .system(system -> system.text(this.systemMessage))
+                .user(user -> user.text(this.userMessage).param("concept", query))
+                .call()
+                .content();
+    }
+    
+}
