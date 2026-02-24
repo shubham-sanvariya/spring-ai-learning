8c1e0b776fc556440b024ee1c087bbd7547e436d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-02-24 21:49:45 +0530

    Add TokenPrintAdvisor class for logging token usage in chat requests

---
+++ lesson2_advisors/src/main/java/com/ai/lesson2_advisors/TokenPrintAdvisor.java
@@ -0,0 +1,53 @@
+package com.ai.lesson2_advisors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+
+import reactor.core.publisher.Flux;
+
+public class TokenPrintAdvisor implements CallAdvisor, StreamAdvisor{
+
+    private Logger logger = LoggerFactory.getLogger(TokenPrintAdvisor.class);
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest arg0, StreamAdvisorChain arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adviseStream'");
+    }
+
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+        logger.info("My Token print advisor is called:");
+
+        logger.info("Request: " + chatClientRequest.prompt().getContents());
+
+        ChatClientResponse chatClientResponse = callAdvisorChain.nextCall(chatClientRequest);
+
+        logger.info("Token Advisor: Response received from the model:");
+
+        logger.info("Response: " + chatClientResponse.chatResponse().getResult().getOutput().getText());
+
+        logger.info("Prompt Token: " + chatClientResponse.chatResponse().getMetadata().getUsage().getPromptTokens());
+        logger.info("Completion Token: " + chatClientResponse.chatResponse().getMetadata().getUsage().getCompletionTokens());
+        logger.info("Total Token consumed: " + chatClientResponse.chatResponse().getMetadata().getUsage().getTotalTokens());
+        
+        return chatClientResponse;
+    }
+
+}
